33d4bc51f0037f4824427e88357b1af1bf833634
ConniConni <nmtgktmo@gmail.com>
2025-05-30 17:14:03 +0900

    005 別解

--- 005_lesson.py
+++ 005_lesson.py
@@ -1,10 +1,24 @@
-row = 10
-col = 5
+# row = 5
+# col = 5
 
-for i in range(row):
-    for j in range(col):
+# for i in range(row):
+#     for j in range(col):
+#         print('●', end='')
+#         # j += 1
+#     print()
+#     # print('\n', end='')
+#     # i += 1
+
+# 別解
+
+row = 5
+col = 5
+i = 0
+
+while i < row:
+    j = 0
+    while j < col:
         print('●', end='')
-        # j += 1
+        j += 1
     print()
-    # print('\n', end='')
-    # i += 1
+    i += 1
